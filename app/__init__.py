b082634d375c0ae7cf1cf9a253e0f649ee9d5f1a
Julen Landa Alustiza <julen@zokormazo.info>
2014-12-30 16:20:06 +0100

    Add error & log handling

--- app/__init__.py
+++ app/__init__.py
@@ -18,8 +18,32 @@ from app.users.models import User
 db_adapter = SQLAlchemyAdapter(db, User)	# Register the user model
 user_manager = UserManager(db_adapter, app)	# Initialize Flask-User
 
+# Setup logging
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Log INFO to a file
+file_handler = RotatingFileHandler('log/flaskllery.log', 'a', 1 * 1024 * 1024, 10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+# Log ERROR to mail in production environments
+if not app.debug:
+	from config import ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD, MAIL_DEFAULT_SENDER
+	from logging.handlers import SMTPHandler
+	credentials = None
+	if MAIL_USERNAME or MAIL_PASSWORD:
+		credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+	mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), MAIL_DEFAULT_SENDER, ADMINS, 'Flaskllery failure', credentials)
+	mail_handler.setLevel(logging.ERROR)
+	app.logger.addHandler(mail_handler)
+
+app.logger.setLevel(logging.INFO)
+app.logger.info('Flaskllery startup')
+
 # import main views
-from app import views
+from app import views, errors
 
 
 
