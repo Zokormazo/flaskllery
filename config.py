923d0f2d9b7b862d61fce601c9e250708f99827b
Julen Landa Alustiza <julen@zokormazo.info>
2015-01-12 16:16:13 +0100

    Fix register

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = '587'
 	MAIL_USE_TLS = True
-	MAIL_DEFAULT_SENDER = ''
+	MAIL_DEFAULT_SENDER = 'no-reply@flask'
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
