dc35d9a1ce6fa09473b2b1b31ce7da3013e4d4b9
Julen Landa Alustiza <julen@zokormazo.info>
2015-01-18 21:19:40 +0100

    localize gallery forms

--- app/gallery/forms.py
+++ app/gallery/forms.py
@@ -4,20 +4,20 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class NewAlbumForm(Form):
-	title = StringField('Title', validators=[DataRequired(), Length(3, 64)])
-	description = StringField('Description', validators=[Length(0, 255)])
+	title = StringField(gettext('Title'), validators=[DataRequired(), Length(3, 64)])
+	description = StringField(gettext('Description'), validators=[Length(0, 255)])
 	submit = SubmitField(gettext('Add'))
 
 class EditAlbumForm(Form):
-	title = StringField('Title', validators=[DataRequired(), Length(3, 64)])
-	description = StringField('Description', validators=[Length(0, 255)])
+	title = StringField(gettext('Title'), validators=[DataRequired(), Length(3, 64)])
+	description = StringField(gettext('Description'), validators=[Length(0, 255)])
 	submit = SubmitField(gettext('Edit'))
 
 class AddDirectoryForm(Form):
-	path = StringField('Path')
+	path = StringField(gettext('Path'))
 	submit = SubmitField(gettext('Add'))
 
 class EditPhotoForm(Form):
-	title = StringField('Title')
-	caption = StringField('Caption')
+	title = StringField(gettext('Title'))
+	caption = StringField(gettext('Caption'))
 	submit = SubmitField(gettext('Edit'))
